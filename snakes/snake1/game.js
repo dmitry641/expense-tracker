b5e0f2b9310e4faf37dd3239a6b0f503f671ac14
Dmitry <dmitry6416@gmail.com>
2020-10-11 23:12:21 +0300

    snake 1.3

--- snakes/snake1/game.js
+++ snakes/snake1/game.js
@@ -2,14 +2,25 @@ import {
   SNAKE_SPEED,
   update as updateSnake,
   draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection,
 } from "./snake.js";
 
 import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
+  if (gameOver) {
+    if (window.confirm("You lost. Press ok to restart")) {
+      window.location = "";
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main); // ~75 times per minute
 
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
@@ -26,10 +37,15 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
--- snakes/snake1/snake.js
+++ snakes/snake1/snake.js
@@ -33,9 +33,20 @@ export function expandSnake(amount) {
   newSegments += amount;
 }
 
-export function onSnake(position) {
-  return snakeBody.some((segment) => equalPositions(segment, position));
+export function getSnakeHead() {
+  return snakeBody[0];
 }
+
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ignoreHead && index === 0) return false;
+    return equalPositions(segment, position);
+  });
+} // тут стало менее понятно
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignoreHead: true });
+} // тут стало менее понятно
 
 function equalPositions(pos1, pos2) {
   return pos1.x === pos2.x && pos1.y === pos2.y;
